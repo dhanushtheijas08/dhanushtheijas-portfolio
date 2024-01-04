a53ca5a2a62ce54b1f1f418c23fa0c7da04bf47c
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-04 15:58:11 +0530

    chore: added new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", "sans-serif"],
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
