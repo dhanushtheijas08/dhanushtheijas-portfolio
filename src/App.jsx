52b5b6b88be6d749665d3fe7c0f3550f1532b36b
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-06 17:24:41 +0530

    feat: add social media links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import SocialAccounts from "./components/SocialAccounts";
 import HeroSection from "./pages/HeroSection";
+
 export default function App() {
   return (
-    <div className="bg-[#030014]">
+    <div className="h-[200vh] bg-[#030014]">
+      <SocialAccounts />
       <HeroSection />
     </div>
   );
