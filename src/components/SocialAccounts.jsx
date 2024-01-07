5d84aacff4d39ecb44e3a2dd5cfe2961d6501054
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-07 20:51:49 +0530

    feat: add icon size

--- src/components/SocialAccounts.jsx
+++ src/components/SocialAccounts.jsx
@@ -7,26 +7,30 @@ const socialAccounts = [
     name: "Twitter",
     link: "https://twitter.com/dhanush_theijas",
     icon: <RiTwitterXFill />,
+    iconSize: "small",
   },
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/dhanush-theijas/",
     icon: <LiaLinkedinIn />,
+    iconSize: "medium",
   },
   {
     name: "GitHub",
     link: "https://github.com/dhanushtheijas08",
     icon: <LiaGithub />,
+    iconSize: "medium",
   },
 ];
 export default function SocialAccounts() {
   const renderSocialAccounts = socialAccounts.map((account, index) => (
-    <li>
+    <li key={account.link}>
       <a
         href={account.link}
-        key={index}
         target="_blank"
-        className="text-xl text-[#a8b2d1] transition-colors duration-200 hover:text-[#f1f0f0]"
+        className={`text-[#a8b2d1] transition-colors duration-200 hover:text-[#f1f0f0] ${
+          account.iconSize === "small" ? "text-xl" : "text-2xl"
+        }`}
       >
         {account.icon}
       </a>
@@ -34,7 +38,7 @@ export default function SocialAccounts() {
   ));
 
   return (
-    <ul className="fixed bottom-5 left-5 z-10 hidden flex-col items-center gap-5 after:block after:h-[90px] after:w-[1.5px] after:bg-[#a8b2d1] lg:flex">
+    <ul className="fixed bottom-5 left-5 z-10 hidden flex-col items-center gap-5  after:block after:h-[90px] after:w-[1.5px] after:bg-[#a8b2d1] lg:flex">
       {renderSocialAccounts}
     </ul>
   );
