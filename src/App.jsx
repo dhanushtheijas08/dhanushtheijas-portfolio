7ff6bf81ba581e5324aeb03d8c4003ac6cf7420d
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-08 15:45:53 +0530

    feat: implement smooth scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
 import SocialAccounts from "./components/SocialAccounts";
 import HeroSection from "./pages/HeroSection";
-
+import Projects from "./pages/Projects";
+import scroll from "./utils/scrollAnimation";
 export default function App() {
+  useEffect(() => {
+    scroll();
+  }, []);
   return (
-    <div className="h-[200vh] bg-[#030014]">
+    <div className="bg-[#030014]">
       <SocialAccounts />
       <HeroSection />
+      <Projects />
     </div>
   );
 }
