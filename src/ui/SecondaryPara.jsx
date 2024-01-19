8b161ffe229c88e036919d3a78830b80f5f20921
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-19 10:14:44 +0530

    refactor: code split for better organization and reusability

---
+++ src/ui/SecondaryPara.jsx
@@ -0,0 +1,11 @@
+export default function SecondaryPara({
+  children,
+  maxWidth = "max-w-xs",
+  classname,
+}) {
+  return (
+    <p className={`text-sm text-white opacity-70  ${maxWidth} ${classname}`}>
+      {children}
+    </p>
+  );
+}
