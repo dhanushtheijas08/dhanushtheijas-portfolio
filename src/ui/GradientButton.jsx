4527db9867e4e67eea0ca1550ca5566dc3cb4312
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-19 11:55:39 +0530

    refactor: split code for reusability

---
+++ src/ui/GradientButton.jsx
@@ -0,0 +1,7 @@
+export default function GradientButton({ children }) {
+  return (
+    <button className="welcome-box border border-[#7042f88b] px-2 py-1.5 opacity-90">
+      {children}
+    </button>
+  );
+}
