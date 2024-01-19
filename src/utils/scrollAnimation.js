aea51bb44144143f64136c47659d88a7c3182a0f
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-19 10:12:08 +0530

    enhance smooth scroll effect with adjusted duration to 1.8

--- src/utils/scrollAnimation.js
+++ src/utils/scrollAnimation.js
@@ -2,7 +2,7 @@ import Lenis from "@studio-freight/lenis";
 
 export default function scroll() {
   const lenis = new Lenis({
-    duration: 1.6,
+    duration: 1.8,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     infinite: false,
     smooth: true,
