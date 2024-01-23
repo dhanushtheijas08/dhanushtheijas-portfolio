98351673db2f27b04c56a735e27d821c0379ad05
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-01-23 11:26:37 +0530

    refactor: change repo in Giscus

--- src/components/GiscusComponent.jsx
+++ src/components/GiscusComponent.jsx
@@ -1,23 +1,21 @@
 import Giscus from "@giscus/react";
 
-export default function GiscusComponent() {
+export default function MyApp() {
   return (
-    <div>
-      <Giscus
-        id="comments"
-        key="comments"
-        repo="dhanushtheijas08/dhanushtheiajs-portfolio"
-        repoId="R_kgDOLAZ8Wg"
-        category="General"
-        categoryId="DIC_kwDOLAZ8Ws4Cch5A"
-        mapping="pathname"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="top"
-        theme="dark"
-        lang="en"
-        loading="lazy"
-      />
-    </div>
+    <Giscus
+      id="comments"
+      repo="dhanushtheijas08/dhanushtheijas-portfolio"
+      repoId="MDEwOlJlcG9zaXRvcnkzOTEzMTMwMjA="
+      category="General"
+      categoryId="DIC_kwDOLAZ8Ws4Cch5B"
+      mapping="specific"
+      term="Welcome to my portfolio"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="dark"
+      lang="en"
+      loading="lazy"
+    />
   );
 }
